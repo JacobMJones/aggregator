c643f632cd41dffd69e307defdb96208172d0338
JacobMJones <mrjacobtaiwan@gmail.com>
2020-01-26 00:59:06 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import prepareData from "./functions/prepareData";
 import Dropdown from "./components/dropdown";
 import setFocus from "./functions/setFocus";
 
-const initialState = {
-  data: [],
-  dataLoaded: false,
-  categories: [],
-  focus: "",
-  focusArray: [],
-  possibleValues: [],
-  focusedValue: "",
-  categoryValueTime: {}
-};
+//state 
+  // data: array
+  // dataLoaded: bool
+  // categories: array
+  // focus: string
+  // focusArray: array
+  // possibleValues: array
+  // focusedValue: string
+  // categoryValueTime: object
+//
 
 function App() {
-  const [state, setState] = useState(initialState);
-
+  const [state, setState] = useState({focus:null});
   useEffect(() => {
     !state.dataLoaded && prepareData(setState);
     setFocus(state, setState);
@@ -24,7 +23,7 @@ function App() {
 
   return (
     <>
-      {!state.data ? (
+      {!state ? (
         "loading"
       ) : (
         <>
@@ -39,14 +38,12 @@ function App() {
             setState={setState}
           />
           <br />
-
-          {/* Displays timestamps related to category(focus) and value(focusedValue) */}
           {/* This needs to be cleaned up. Hack way of dealing with when a focus and focusValue don't match */}
           {state.focusedValue &&
             state.categoryValueTime[state.focus][state.focusedValue] &&
             state.categoryValueTime[state.focus][state.focusedValue].map(
               (item, index) => {
-                return <div key={"time" + index}>{index}. {item}</div>;
+                return <div key={"time" + index}>{item}</div>;
               }
             )}
         </>
--- src/functions/setFocus.js
+++ src/functions/setFocus.js
@@ -1,16 +1,15 @@
 import dataPrepFunctions from "./dataPrepFunctions";
 
 export default function setFocus(state, setState) {
-  
   const tempPossible = [];
-
-  for (const rec in state.categoryValueTime[state.focus]) {
-    tempPossible.push(rec);
+  if (state.categoryValueTime) {
+    for (const rec in state.categoryValueTime[state.focus]) {
+      tempPossible.push(rec);
+    }
+    const possibleValues = tempPossible.filter(dataPrepFunctions.onlyUnique);
+    setState(prev => ({
+      ...prev,
+      possibleValues
+    }));
   }
-
-  const possibleValues = tempPossible.filter(dataPrepFunctions.onlyUnique);
-  setState(prev => ({
-    ...prev,
-    possibleValues
-  }));
 }
